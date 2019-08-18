a57ee94d2bb24edc9dc5479fddc55bb082a5ad15
muhwezi deo <aggrey256@gmail.com>
2019-08-18 16:04:11 +0300

    setup simple client

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const accountSid = process.env.AccountSID
 const authToken = process.env.AuthToken
 const client = require('twilio')(accountSid, authToken);
+const user = 'Dee';
 
 client.messages.create({
     from: 'whatsapp:+14155238886',
-    body: 'Hello there!',
-    to: 'whatsapp:+256705495719'
+    body: `
+    Hello ${user}  this Phone number ${process.env.Phone}
+    Has an upcomming trip ${new Date()}
+    `,
+    to: `whatsapp:${process.env.Phone}`
 })
 .then(message => console.log(message))
 .catch(e => console.log(e));
+
